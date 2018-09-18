452a44744d3bbb902d52f82c72252dad5bb3a8cb
Willen Oliveira Leal <43154735+willenrvstudent@users.noreply.github.com>
2018-09-17 20:28:12 -0400

    Change

--- Picture.java
+++ Picture.java
@@ -21,7 +21,7 @@ public class Picture
      */
     public Picture()
     {
-        // nothing to do... instance variables are automatically set to null
+        
     }
 
     /**
@@ -55,7 +55,76 @@ public class Picture
         sun.changeSize(80);
         sun.makeVisible();
     }
-
+    public void draw_Leal()
+    {
+        if(!drawn) {
+            sand.moveHorizontal(-310);
+            sand.changeColor("sand");
+            sand.moveVertical(20);
+            sand.changeSize(550);
+            sand.makeVisible();
+            
+            sky.moveHorizontal(-320);
+            sky.changeColor("skyBlue");
+            sky.moveVertical(-520);
+            sky.changeSize(550);
+            sky.makeVisible();
+            
+            balloon.changeColor("red");
+            balloon.moveHorizontal(45);
+            balloon.moveVertical(45);
+            balloon.changeSize(70);
+            balloon.makeVisible();
+            
+            person.moveHorizontal(30);
+            person.moveVertical(40);
+            person.changeSize(20, 15);
+            person.makeVisible();
+            
+            sqr1.moveHorizontal(-20);
+            sqr1.changeColor("blue");
+            sqr1.moveVertical(90);
+            sqr1.changeSize(5);
+            sqr1.makeVisible();
+            
+            sqr2.moveHorizontal(-15);
+            sqr2.changeColor("blue");
+            sqr2.moveVertical(105);
+            sqr2.changeSize(5);
+            sqr2.makeVisible();
+            
+            sqr3.moveHorizontal(15);
+            sqr3.changeColor("blue");
+            sqr3.moveVertical(90);
+            sqr3.changeSize(5);
+            sqr3.makeVisible();
+            
+            sqr4.moveHorizontal(10);
+            sqr4.changeColor("blue");
+            sqr4.moveVertical(105);
+            sqr4.changeSize(5);
+            sqr4.makeVisible();
+    
+            basket.changeColor("blue");
+            basket.moveHorizontal(-10);
+            basket.moveVertical(120);
+            basket.changeSize(20);
+            basket.makeVisible();
+    
+            pyramid.changeSize(70, 90);
+            pyramid.changeColor("brown");
+            pyramid.moveHorizontal(-115);
+            pyramid.moveVertical(-30);
+            pyramid.makeVisible();
+    
+            sun.changeColor("yellow");
+            sun.moveHorizontal(120);
+            sun.moveVertical(-60);
+            sun.changeSize(50);
+            sun.makeVisible();
+            drawn = true;
+        }
+    }
     /**
      * Change this picture to black/white display
      */
